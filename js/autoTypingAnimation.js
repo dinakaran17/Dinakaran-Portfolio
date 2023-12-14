c42bcd8195c84cea4a993e12db45f59f66406ffa
Dinakaran J <103357851+dinakaran17@users.noreply.github.com>
2023-12-15 01:25:47 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span> Hi, I am Dinakaran </span> To seek a job to fulfill my career dreams, enhance my knowledge innovatively, and be a responsible, smart working candidate."
+      ],
+      typeSpeed:2,
+      contentType: 'html'
+    });
+  }, 100);
